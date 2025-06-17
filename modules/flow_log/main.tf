97ac1a0a58186cf9103abe00c7cb4a14c2972871
mrossco-bssprx <mike@bssprx.com>
2025-06-17 17:05:38 -0500

    add enabled flag, configurable log_group_name_prefix, validations for log_group_arn and iam_role_arn, and new log_group_arn output.  Also manage creation of a log_group if no arn is passed in, otherwise attach the flow logs to an existing arn

--- modules/flow_log/main.tf
+++ modules/flow_log/main.tf
@@ -22,13 +22,28 @@ variable "eni_id" {
 }
 
 variable "log_group_arn" {
-  description = "ARN of the CloudWatch log group"
+  description = "ARN of the CloudWatch log group. If not provided, a log group will be created."
   type        = string
+  default     = null
+  validation {
+    condition     = var.log_group_arn == null || can(regex("^arn:aws:logs:", var.log_group_arn))
+    error_message = "log_group_arn must start with 'arn:aws:logs:' if provided."
+  }
+}
+
+variable "log_group_name_prefix" {
+  description = "Prefix for the CloudWatch log group name if created"
+  type        = string
+  default     = "vpc-flow-"
 }
 
 variable "iam_role_arn" {
   description = "IAM role ARN to use for delivering flow logs"
   type        = string
+  validation {
+    condition     = can(regex("^arn:aws:iam::", var.iam_role_arn))
+    error_message = "iam_role_arn must start with 'arn:aws:iam::'."
+  }
 }
 
 variable "tags" {
@@ -43,6 +58,22 @@ variable "target_resource" {
   default     = null
 }
 
+variable "retention_in_days" {
+  description = "Number of days to retain log events in the CloudWatch log group"
+  type        = number
+  default     = null
+  validation {
+    condition     = var.retention_in_days == null || contains([1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653], var.retention_in_days)
+    error_message = "Retention must be one of the allowed values per AWS documentation, or null to use default."
+  }
+}
+
+variable "enabled" {
+  description = "Whether to enable the flow log resources"
+  type        = bool
+  default     = true
+}
+
 locals {
   targets_set = [
     var.vpc_id != null,
@@ -51,8 +82,16 @@ locals {
   ]
 }
 
+resource "aws_cloudwatch_log_group" "this" {
+  count             = var.enabled && var.log_group_arn == null ? 1 : 0
+  name_prefix       = var.log_group_name_prefix
+  retention_in_days = var.retention_in_days != null ? var.retention_in_days : 30
+  tags              = var.tags
+}
+
 resource "aws_flow_log" "this" {
-  log_destination      = var.log_group_arn
+  count                = var.enabled ? 1 : 0
+  log_destination      = var.log_group_arn != null ? var.log_group_arn : aws_cloudwatch_log_group.this[0].arn
   log_destination_type = "cloud-watch-logs"
   iam_role_arn         = var.iam_role_arn
   traffic_type         = var.traffic_type
@@ -61,6 +100,10 @@ resource "aws_flow_log" "this" {
   vpc_id    = var.vpc_id != null ? var.vpc_id : null
   subnet_id = var.subnet_id != null ? var.subnet_id : null
   eni_id    = var.eni_id != null ? var.eni_id : null
+
+  depends_on = [
+    aws_cloudwatch_log_group.this
+  ]
 
   lifecycle {
     precondition {
@@ -72,16 +115,20 @@ resource "aws_flow_log" "this" {
 
 output "id" {
   description = "The ID of the flow log"
-  value       = aws_flow_log.this.id
+  value       = var.enabled ? aws_flow_log.this[0].id : null
 }
 
 output "arn" {
   description = "The ARN of the flow log"
-  value       = aws_flow_log.this.arn
+  value       = var.enabled ? aws_flow_log.this[0].arn : null
 }
 
-variable "retention_in_days" {
-  description = "Number of days to retain log events in the CloudWatch log group"
-  type        = number
-  default     = null
+output "log_group_name" {
+  description = "Name of the CloudWatch log group used by the flow log"
+  value       = var.enabled && var.log_group_arn == null ? aws_cloudwatch_log_group.this[0].name : null
+}
+
+output "log_group_arn" {
+  description = "ARN of the CloudWatch log group used by the flow log"
+  value       = var.enabled ? (var.log_group_arn != null ? var.log_group_arn : aws_cloudwatch_log_group.this[0].arn) : null
 }
