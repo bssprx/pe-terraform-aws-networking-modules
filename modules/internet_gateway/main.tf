b129fea626b74e70d395aaac0b7463ed1368b9a0
mrossco-bssprx <mike@bssprx.com>
2025-06-17 17:14:43 -0500

    improve module documentation and validation clarity

--- modules/internet_gateway/main.tf
+++ modules/internet_gateway/main.tf
@@ -1,14 +1,32 @@
 variable "vpc_id" {
-  type = string
+  type        = string
+  description = "The ID of the VPC to associate with the Internet Gateway."
+  validation {
+    condition     = length(trim(var.vpc_id, " ")) > 0
+    error_message = "The vpc_id variable must be a non-empty string."
+  }
 }
 
 variable "name_prefix" {
-  type = string
+  type        = string
+  description = "Prefix to use for naming the Internet Gateway."
+  validation {
+    condition     = length(trim(var.name_prefix, " ")) > 0
+    error_message = "The name_prefix variable must be a non-empty string."
+  }
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
+  description = "Tags to apply to the Internet Gateway resource."
+  validation {
+    condition = alltrue([
+      for k, v in var.tags :
+      length(trim(k, " ")) > 0 && length(trim(v, " ")) > 0
+    ])
+    error_message = "All tag keys and values must be non-empty strings."
+  }
 }
 
 resource "aws_internet_gateway" "this" {
@@ -23,5 +41,6 @@ resource "aws_internet_gateway" "this" {
 }
 
 output "id" {
-  value = aws_internet_gateway.this.id
+  value       = aws_internet_gateway.this.id
+  description = "The ID of the Internet Gateway."
 }
