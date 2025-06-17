ca2e083d091250fe9616c75e4c6bcdd2c7315bc2
mrossco-bssprx <mike@bssprx.com>
2025-06-17 17:08:34 -0500

    ensure name and role are non-empty strings and that each item in statements includes the required keys

--- modules/iam_policy/main.tf
+++ modules/iam_policy/main.tf
@@ -1,16 +1,31 @@
 variable "name" {
   description = "Name of the policy"
   type        = string
+
+  validation {
+    condition     = length(trim(var.name, " ")) > 0
+    error_message = "Policy name must be a non-empty string."
+  }
 }
 
 variable "role" {
   description = "IAM role name to attach the policy to"
   type        = string
+
+  validation {
+    condition     = length(trim(var.role, " ")) > 0
+    error_message = "IAM role name must be a non-empty string."
+  }
 }
 
 variable "statements" {
   description = "List of policy statements"
-  type        = list(any)
+  type        = list(map(any))
+
+  validation {
+    condition     = alltrue([for s in var.statements : alltrue([for k in ["Effect", "Action", "Resource"] : contains(keys(s), k)])])
+    error_message = "Each policy statement must include 'Effect', 'Action', and 'Resource' keys."
+  }
 }
 
 variable "enabled" {
