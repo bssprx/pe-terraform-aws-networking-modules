c873c0cb9477e73bf8300fb6ddf39c3dc9b3cff8
mrossco-bssprx <mike@bssprx.com>
2025-06-17 17:16:28 -0500

    add validation to ensure the tags variable includes both Environment and Project

--- modules/nat_gateway/main.tf
+++ modules/nat_gateway/main.tf
@@ -7,6 +7,11 @@ variable "tags" {
   description = "Tags to apply to the NAT Gateway and associated resources"
   type        = map(string)
   default     = {}
+
+  validation {
+    condition     = alltrue([for k in ["Environment", "Project"] : contains(keys(var.tags), k)])
+    error_message = "The 'tags' map must include both 'Environment' and 'Project' keys."
+  }
 }
 
 variable "name_prefix" {
@@ -43,6 +48,6 @@ resource "aws_nat_gateway" "this" {
 }
 
 output "nat_gateway_id" {
-  description = "The ID of the NAT Gateway"
+  description = "The ID of the NAT Gateway resource created in this module. This value can be used to reference the NAT Gateway for routing or other module integrations."
   value       = aws_nat_gateway.this.id
 }
