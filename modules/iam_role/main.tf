b419a6f8242f2ee6843d36bdd8f94d85fe840d33
mrossco-bssprx <mike@bssprx.com>
2025-06-18 16:21:04 -0500

    conditionally include Condition only if it exists and is not null in the policy_statements

--- modules/iam_role/main.tf
+++ modules/iam_role/main.tf
@@ -77,7 +77,7 @@ resource "aws_iam_role_policy" "this" {
           Action   = s.action
           Resource = s.resource
         },
-        s.condition != null ? { Condition = s.condition } : {}
+        contains(keys(s), "condition") && s.condition != null ? { Condition = s.condition } : {}
       )
     ]
   })
